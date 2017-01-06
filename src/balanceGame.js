13f9a21fa4c9107b9b992ee6f18e7f44929ce418
Kirill Grafov <countjr@gmail.com>
2017-01-06 09:53:17 -0600

    add balance game

--- src/balanceGame.js
+++ src/balanceGame.js
@@ -3,16 +3,16 @@
 import game from './index';
 import { randomNumber } from './functions';
 
-const makeBalanseNumber = (number) => {
+const makeBalanseNumber = (number: string) => {
   const sortedLine = number.split('').sort().join('');
   const lastIndex = sortedLine.length - 1;
-  if (sortedLine[lastIndex] - sortedLine[0] <= 1) {
+  if (+sortedLine[lastIndex] - +sortedLine[0] <= 1) {
     return sortedLine;
   }
   const firstNumber = [+sortedLine.split('')[0] + 1];
   const lastNumber = sortedLine.split('')[lastIndex] - 1;
   const middlePart = sortedLine.slice(1, lastIndex);
-  const result = firstNumber.concat(middlePart, lastNumber).sort().join('');
+  const result = firstNumber.concat(middlePart, lastNumber).join('');
   return makeBalanseNumber(result);
 };
 
